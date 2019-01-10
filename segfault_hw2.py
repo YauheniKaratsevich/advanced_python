fea42ae43a1a9b6b21451e54aeb66468edebcd11
Yauheni UL20A <evgen.korotsevich@gmail.com>
2019-01-10 13:30:58 +0300

    HW2: get a segfault

--- segfault_hw2.py
+++ segfault_hw2.py
@@ -1,5 +1,5 @@
-#Get a SEGFAULT
-#Idea: print a value by random address
+# Get a SEGFAULT
+# Idea: print a value by random address
 import ctypes
 
 print(ctypes.cast(123456789, ctypes.py_object).value)
